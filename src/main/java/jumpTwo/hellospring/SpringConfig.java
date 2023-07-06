48a4a813451f796e0c9b23bd63a35c52e6b081d6
azsx92 <azsx92@gmail.com>
2023-07-06 22:05:14 +0900

    jdbcTemplate

--- src/main/java/jumpTwo/hellospring/SpringConfig.java
+++ src/main/java/jumpTwo/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package jumpTwo.hellospring;
 
 import jumpTwo.hellospring.repository.JdbcMemberRepository;
+import jumpTwo.hellospring.repository.JdbcTemplateMemberRepository;
 import jumpTwo.hellospring.repository.MemberRepository;
 import jumpTwo.hellospring.repository.MemoryMemberRepository;
 import jumpTwo.hellospring.service.MemberService;
@@ -12,16 +13,6 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-/*    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }*/
-
-
     private final DataSource dataSource;
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -32,7 +23,8 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+       // return new MemoryMemberRepository();
+      //  return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
--- src/test/java/jumpTwo/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/jumpTwo/hellospring/service/MemberServiceIntegrationTest.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
 public class MemberServiceIntegrationTest {
@@ -25,25 +27,29 @@ public class MemberServiceIntegrationTest {
 
         //Given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring2");
 //When
         Long saveId = memberService.join(member);
 //Then
-        Member findMember = memberRepository.findByID(saveId).get();
-        assertEquals(member.getName(), findMember.getName());
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
     @Test
     public void 중복_회원_예외() throws Exception {
 //Given
         Member member1 = new Member();
-        member1.setName("spring");
+        member1.setName("spring2");
         Member member2 = new Member();
-        member2.setName("spring");
+        member2.setName("spring2");
 //When
+
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class,
-                () -> memberService.join(member2));//예외가 발생해야 한다. assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
     }
 }
 
